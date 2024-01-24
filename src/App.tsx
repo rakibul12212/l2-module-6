00e65bfa8df1ac1a10a6275228243eab7652e98f
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-25 02:22:01 +0600

    4 type of buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import "./App.css";
 const App = () => {
   return (
     <div className="main">
-      <div className="box-1">box-1</div>
+      {/* <div className="box-1">box-1</div> */}
       {/* <h1 className="text-2xl">something cool</h1> */}
+      <button className="btn">Hover Me</button>
     </div>
   );
 };
