4b77e5d1fa44b7ae01bcabb587598bc3bc1a3efd
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-25 04:06:58 +0600

    create staggered animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import "./App.css";
 
 const App = () => {
+  const name = "Programming Hero";
+  const nameArr = name.split("");
   return (
-    <div className="main ">
+    <div className="main">
       {/* <div className="box-1">box-1</div> */}
       {/* <h1 className="text-2xl">something cool</h1> */}
       {/* button */}
@@ -14,7 +16,7 @@ const App = () => {
         <div className="box"></div>
       </div> */}
       {/* clip-path */}
-      <div className="card">
+      {/* <div className="card">
         <h1 className="text-xl font-bold">Heading</h1>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt,
@@ -22,6 +24,15 @@ const App = () => {
           veritatis! At necessitatibus ipsa placeat quod dolor. Expedita veniam
           cumque commodi labore.
         </p>
+      </div> */}
+
+      {/* staggered*/}
+      <div className="container">
+        {nameArr.map((item, i) => (
+          <span className="alphabet" style={{ transitionDelay: `${i * 30}ms` }}>
+            {item}
+          </span>
+        ))}
       </div>
     </div>
   );
