23593da4707713fcae761544c10512b1bf0c26d5
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-25 03:28:08 +0600

    use of clip-path

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,22 @@ const App = () => {
     <div className="main ">
       {/* <div className="box-1">box-1</div> */}
       {/* <h1 className="text-2xl">something cool</h1> */}
+      {/* button */}
       {/* <button className="btn">Hover Me</button> */}
-      <div className="loading">
+      {/* loader */}
+      {/* <div className="loading">
         <div className="glass"></div>
         <div className="box"></div>
+      </div> */}
+      {/* clip-path */}
+      <div className="card">
+        <h1 className="text-xl font-bold">Heading</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nesciunt,
+          doloribus nulla deserunt ipsum ducimus dolore officia ipsam nobis
+          veritatis! At necessitatibus ipsa placeat quod dolor. Expedita veniam
+          cumque commodi labore.
+        </p>
       </div>
     </div>
   );
