72259035dbb08071114f0faf1127b30416c68113
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-25 03:12:38 +0600

    animated loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import "./App.css";
 
 const App = () => {
   return (
-    <div className="main m-48">
-      <div className="box-1">box-1</div>
+    <div className="main ">
+      {/* <div className="box-1">box-1</div> */}
       {/* <h1 className="text-2xl">something cool</h1> */}
       {/* <button className="btn">Hover Me</button> */}
+      <div className="loading">
+        <div className="glass"></div>
+        <div className="box"></div>
+      </div>
     </div>
   );
 };
