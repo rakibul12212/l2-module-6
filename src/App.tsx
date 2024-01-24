e171980e2b341c506b4120647fb5dfa719f2bd9d
rakibul12212 <rakibul15-12212@diu.edu.bd>
2024-01-24 19:26:48 +0600

    flip

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="main">
       <div className="box-1">box-1</div>
-      <h1 className="text-2xl">something cool</h1>
+      {/* <h1 className="text-2xl">something cool</h1> */}
     </div>
   );
 };
